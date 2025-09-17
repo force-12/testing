f7f507dd7f9146f1223e378f4c661ca7d89a3039
Muhammad Irfan Rahman <65913477+force-12@users.noreply.github.com>
2025-09-17 20:53:06 +0700

    Update login.py

--- login.py
+++ login.py
@@ -24,7 +24,7 @@ if st.button("Login"):
             if result:
                 st.session_state['logged_in'] = True
                 st.success("Login berhasil!")
-                st.switch_page("pages/app.py")
+                st.switch_page("app.py")
             else:
                 st.error("Username / Password salah.")
         except Exception as e:
@@ -34,4 +34,5 @@ if st.button("Login"):
             conn.close()
 
 if st.button("Daftar jika belum ada akun"):
-    st.switch_page("pages/daftar.py")
+    st.switch_page("daftar.py")
+
